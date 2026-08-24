5a0a75b46638569270ec67be1eac5f4d18a88cc3
Andy Peniak <232211226+AndrianaPeniak@users.noreply.github.com>
2026-08-24 12:54:12 +0300

    Added user choice function.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function main() {
 
     while (i < 5) {
         console.log(`Round ${i}`);
+        const humanChoice = getHumanChoice();
+        console.log(`Your choice: ${humanChoice}`);
         const computerChoice = getComputerChoice();
         console.log(`Computer choice: ${computerChoice}`);
         i++;
@@ -15,6 +17,21 @@ function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
     const computerChoice = choices[Math.floor(choices.length * Math.random())];
     return computerChoice;
+}
+
+function getHumanChoice(){
+    let isValid = false;
+    while (isValid == false) {
+        let answer = prompt("Your choice:");
+        answer = answer.toLowerCase();
+        if (answer == "rock" || answer == "scissors" || answer == "paper"){
+            return answer;
+            isValid = true;
+        } else {
+            console.log("Can't recognise your choice.");
+            continue;
+        }
+    }
 }
 
 main()
