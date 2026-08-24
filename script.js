0626d7c8d6915aaa52fd4e1004993e78d02cd330
Andy Peniak <232211226+AndrianaPeniak@users.noreply.github.com>
2026-08-24 12:21:20 +0300

    Added function that randomly chooses rock, paper, or scissors.

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function main() {
 
     while (i < 5) {
         console.log(`Round ${i}`);
+        const computerChoice = getComputerChoice();
+        console.log(`Computer choice: ${computerChoice}`);
         i++;
     }
 }
+
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(choices.length * Math.random())];
+    return computerChoice;
+}
+
 main()
